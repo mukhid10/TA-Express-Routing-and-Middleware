a942a5d02906b2e81c56aace769e1c740440b724
mukhid10 <mukhid976@gmail.com>
2022-03-26 13:12:40 +0700

    TA- Routing Midleware

---
+++ soal-2/index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const hewan = [
+    {id: 1, nama: 'Snowy', spesies: 'kucing'},
+    {id: 2, nama: 'Blacki', spesies: 'anjing'},
+    {id: 3, nama: 'Molly', spesies: 'kucing'},
+    {id: 4, nama: 'Milo', spesies: 'kelinci'},
+    {id: 5, nama: 'Rere', spesies: 'kucing'},
+  ]
+
+  const logger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+  }
+  
+  app.use(logger)
+
+app.get('/hewan', (req, res) => {
+  res.send(hewan)
+})
+
+app.get('/hewan/:id', (req, res) => {
+    const id = req.params.id
+    res.send(hewan.id)
+  })
+
+app.post('/hewan', (req, res) => {
+    res.send('ini post')
+  })
+
+app.put('/hewan/:id', (req, res) => {
+    const id = req.params.id
+    hewan.filter(kewan =>{
+        if (kewan.id == id) {
+            kewan.id = id
+            kewan.nama = req.body.nama
+            kewan.spesies = req.body.spesies
+
+            return kewan
+        }
+    })
+    res.json(hewan)
+  })
+
+app.delete('/hewan/:id', (req, res) => {
+    const id = req.params.id
+    hewan = hewan.filter(kewan => kewan != id)
+    res.send(hewan)
+  })
+
+app.listen(port, () => {
+  console.log(`Success ${port}`)
+})
